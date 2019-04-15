a94e63fbaf347fccce069874e49f98b0c6030ce2
positive-jinho <red_horse_@naver.com>
2019-04-15 15:53:35 +0900

    Updates

---
+++ precache-manifest.41ccdb299e38818899b6f99df4119574.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2acc8d77ba1ab1c0749a",
+    "url": "/crop-at-once/static/js/runtime~main.1b4d1331.js"
+  },
+  {
+    "revision": "0912d10daf47d1921691",
+    "url": "/crop-at-once/static/js/main.d4487907.chunk.js"
+  },
+  {
+    "revision": "111489ccd44b2a027707",
+    "url": "/crop-at-once/static/js/2.eb9a0e9a.chunk.js"
+  },
+  {
+    "revision": "d0fccda8e413d6a57ad5fd566e4d98bd",
+    "url": "/crop-at-once/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cao/precache-manifest.0fb84cfa5334e9438c3567b93ccfa230.js"
+  "/crop-at-once/precache-manifest.41ccdb299e38818899b6f99df4119574.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cao/index.html", {
+workbox.routing.registerNavigationRoute("/crop-at-once/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
